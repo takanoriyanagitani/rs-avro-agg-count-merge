6d06f84b6a19cf36dfdfc467a3c5f59178fe95cc
yanagitani <takanori.yanagitani@gmail.com>
2025-02-01 07:43:52 +0900

    chore: simple helper shells

---
+++ build-wasi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export RUSTFLAGS="-Ctarget-feature=+simd128"
+
+cargo \
+	build \
+	--target wasm32-wasip1 \
+	--profile release-wasi \
+	--no-default-features \
+	--features enable_snappy
